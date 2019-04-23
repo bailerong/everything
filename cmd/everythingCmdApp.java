9290fa245a4c58b2a2cff0e2624bb8a526071d16
bailerong <15929276532@163.com>
2019-04-23 22:48:20 +0800

    这是一个我们的程序入口

---
+++ cmd/everythingCmdApp.java
@@ -0,0 +1,93 @@
+package com.lele.everything.cmd;
+
+import com.lele.everything.Core.Condition;
+import com.lele.everything.Core.EverythingPlusManager;
+import com.lele.everything.Core.Thing;
+
+import java.util.List;
+import java.util.Scanner;
+
+
+public class everythingCmdApp {
+    private static Scanner scanner=new Scanner(System.in);
+
+    public static void main(String[] args) {
+        welcom();
+        //统一调度器
+        EverythingPlusManager manager=EverythingPlusManager.getInstance();
+    //交互式
+        interactive(manager);
+    }
+    private static void interactive(EverythingPlusManager manager){
+        while(true){
+            System.out.println("everything>>");
+            String input=scanner.nextLine();
+            if(input.startsWith("search")){
+                String[] values=input.split(" ");
+                if(values.length>=2) {
+                    if (!values[0].equals("search")) {
+                        help();
+                        continue;
+                    }
+                    Condition condition = new Condition();
+                    String name = values[1];
+                    condition.setName(name);
+                    if (values.length >= 3) {
+                        String filetype = values[2];
+                        condition.setFileType(filetype.toUpperCase());
+                    }
+                    search(manager, condition);
+                        continue;
+                }else{
+                        help();
+                        continue;
+                    }
+                }
+            switch (input){
+                case"help":
+                    help();
+                    break;
+                case"quit":
+                    quit();
+                    break;
+                case"index":
+                    index(manager);
+                    break;
+
+                    default:
+                        help();
+            }
+        }
+    }
+    private static void search(EverythingPlusManager manager,Condition condition){
+        System.out.println("检索功能");
+        //统一调度器中的searchhelp
+        List<Thing> thingList= manager.search(condition);
+        for(Thing ting:thingList){
+            System.out.println(ting.getPath());
+        }
+
+    }
+    private static void index(EverythingPlusManager manager){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                manager.buildIndex();
+            }
+        });
+    }
+    private static void quit(){
+        System.out.println("程序退出");
+        return;
+    }
+    private static void welcom(){
+        System.out.println("欢迎使用，Everything：");
+    }
+private static void help(){
+    System.out.println("help==>帮助");
+    System.out.println("index==>检索");
+    System.out.println("search==>查询");
+    System.out.println("quit===>退出");
+
+}
+}
