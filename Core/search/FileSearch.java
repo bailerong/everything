9d932553ee80e029ba76ff858737352e750f8aee
bailerong <38148426+bailerong@users.noreply.github.com>
2019-08-23 23:54:29 +0800

    Update FileSearch.java

--- Core/search/FileSearch.java
+++ Core/search/FileSearch.java
@@ -5,6 +5,7 @@ import com.lele.everything.Core.Thing;
 //一个接口
 import java.util.List;
 /*我们的文件检索*/
+
 public interface FileSearch {
     //根据condition条件进行数据库检索
     List<Thing> search(Condition condition);
